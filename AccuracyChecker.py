ee86ca18edf6ff306fc2fe6de993507f27dd2ac0
Karthikeyan Thangavel <tkchennai89@gmail.com>
2013-11-17 20:36:29 -0500

    Verify accuracy given predicted and actual label files<Karthik>

---
+++ AccuracyChecker.py
@@ -0,0 +1,21 @@
+import sys
+import os
+
+parameters = sys.argv
+if len(parameters) > 1:
+    predictedLabelFileName = parameters[1]
+    trueLabelFileName = parameters[2]
+
+baseDirectory = os.path.dirname(os.path.realpath(__file__))
+predictedLabelFile = open(os.path.join(baseDirectory,"Data/" + predictedLabelFileName), 'r')
+trueLabelFile = open(os.path.join(baseDirectory,"Data/" + trueLabelFileName), 'r')
+
+i = 0;
+match = 0;
+for line in predictedLabelFile:
+    if line == trueLabelFile.readline():
+        match += 1
+    i += 1
+
+print "Accuracy - "
+print float(match)/i
