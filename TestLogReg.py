dd0e951cbf5b265660f4802f6c9965691802d108
Karthikeyan Thangavel <tkchennai89@gmail.com>
2013-11-20 15:15:09 -0500

    Applying generated model to work on test data. Added comments<Karthik>

--- TestLogReg.py
+++ TestLogReg.py
@@ -2,26 +2,40 @@ import sys
 import os
 import math
 
+#default values
+modelFileName = "model.dat"
+testingFeatureFileName = "testFeature.dat"
+predictedLabelFileName = "predictedLabels.dat"
+
 parameters = sys.argv
+#override with command-line parameters
 if len(parameters) > 1:
     modelFileName = parameters[1]
     testingFeatureFileName = parameters[2]
     predictedLabelFileName = parameters[3]
 
+#Open the specified files
 baseDirectory = os.path.dirname(os.path.realpath(__file__))
 testingFeatureFile = open(os.path.join(baseDirectory,"Data/" + testingFeatureFileName), 'r')
 modelFile = open(os.path.join(baseDirectory,"Data/" + modelFileName), 'r')
 predictedLabelFile = open(os.path.join(baseDirectory,"Data/" + predictedLabelFileName), 'w')
 
 features = []
-w = []
+#Initialize the test feature vectors from file
 for line in testingFeatureFile:
     features.append([int(s) for s in line.split(' ')])
+testingFeatureFile.close()
+
+w = []
+#Initialize the weight vector from model file
 for line in modelFile:    
     w.append(float(line))
+modelFile.close()
 
-h=[]
 for i in range(len(features)):
     wx = sum([a*b for a,b in zip(w,features[i])])
-    h.append(1.0 / (1 + math.exp(-wx)))
-print h            
+    sig = (1.0 / (1 + math.exp(-wx)))
+    if sig > 0.5:
+        predictedLabelFile.write("1\n")
+    else:
+        predictedLabelFile.write("0\n")        
--- TrainLogReg.py
+++ TrainLogReg.py
@@ -2,7 +2,16 @@ import sys
 import os
 import math
 
+#default values
+trainingFeatureFileName = "trainingFeature.dat"
+trainingLabelFileName = "trainingLabel.dat"
+modelFileName = "model.dat"
+dimensions = 785
+lambdaValue = 0.00001
+nIter = 1
+
 parameters = sys.argv
+#override with command-line parameters
 if len(parameters) > 1:
     trainingFeatureFileName = parameters[1]
     trainingLabelFileName = parameters[2]
@@ -11,6 +20,7 @@ if len(parameters) > 1:
     lambdaValue = float(parameters[5])
     nIter = int(parameters[6])
 
+#Open the specified files
 baseDirectory = os.path.dirname(os.path.realpath(__file__))
 trainingFeatureFile = open(os.path.join(baseDirectory,"Data/" + trainingFeatureFileName), 'r')
 trainingLabelFile = open(os.path.join(baseDirectory,"Data/" + trainingLabelFileName), 'r')
@@ -18,10 +28,16 @@ modelFile = open(os.path.join(baseDirectory,"Data/" + modelFileName), 'w')
 
 features = []
 labels = []
+#Initialize the feature vectors and labels from files
 for line in trainingFeatureFile:
     features.append([int(s) for s in line.split(' ')])
     labels.append(int(trainingLabelFile.readline()))
-        
+
+#Close the file once data is read    
+trainingFeatureFile.close()
+trainingLabelFile.close()
+    
+#Logistic regression
 t = 0
 w = [0] * dimensions
 for i in range(len(features)):
@@ -33,5 +49,6 @@ for i in range(len(features)):
     wMultiplier = float(lambdaValue / t)
     w = [w[i] - dw[i]*wMultiplier for i in range(dimensions)]
 
+#write the calculated weights to the model file
 for element in w:
     modelFile.write(str(element) + "\n")                
